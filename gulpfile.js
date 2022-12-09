c514e4c3d256cb0595ddd108b340b798a3b307fa
Satetsu-Hokkairo <satetu.hokkairo@gmail.com>
2022-12-09 14:50:53 +0900

    add gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,54 @@
-// gulpプラグインの読み込み
-const gulp = require("gulp");
-// Sassをコンパイルするプラグインの読み込み
-const sass = require("gulp-sass")(require("sass"));
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
 
-// style.scssをタスクを作成する
-gulp.task("default", () => {
+// gulpプラグインを読み込みます
+const { src, dest, series, parallel, watch } = require("gulp");
+// Sassをコンパイルするプラグインを読み込みます
+const sass = require("gulp-sass")(require("sass"));
+const imageMin = require("gulp-imagemin");
+const mozjpeg = require("imagemin-mozjpeg"); // 追加
+const pngquant = require("imagemin-pngquant"); // 追加
+const changed = require("gulp-changed");
+
+/**
+ * Sassをコンパイルするタスクです
+ */
+const compileSass = () =>
   // style.scssファイルを取得
-  return (
-    gulp
-      .src("styles/style.scss")
-      // Sassのコンパイルを実行
-      .pipe(
-        sass({
-          outputStyle: "compressed"
-        })
-      )
-      // cssフォルダー以下に保存
-      .pipe(gulp.dest("css"))
-  );
-});
+  src("styles/*.scss")
+    // Sassのコンパイルを実行
+    .pipe(
+      // コンパイル後のCSSを展開
+      sass({
+        outputStyle: "compressed",
+      })
+    )
+    // cssフォルダー以下に保存
+    .pipe(dest("styles/css"));
+function imagemin(done) {
+  src("images/**")
+    .pipe(changed("images/"))
+    .pipe(
+      imageMin([
+        pngquant({
+          // 追加
+          quality: [0.6, 0.7],
+          speed: 1,
+        }),
+        mozjpeg({ quality: 65 }), // 追加
+        imageMin.svgo(),
+        imageMin.optipng(),
+        imageMin.gifsicle({ optimizationLevel: 3 }),
+      ])
+    )
+    .pipe(dest("images/image/"));
+  done();
+}
+/**
+ * Sassファイルを監視し、変更があったらSassを変換します
+ */
+const watchSassFiles = () => watch("styles/*/_*.scss", compileSass);
+
+// npx gulpというコマンドを実行した時、watchSassFilesが実行されるようにします
+exports.default = watchSassFiles;
+exports.imagemin = imagemin;
